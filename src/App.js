4f9275d96911e263231bb30196caa7d0712761a6
smarist <stmarisnwokike@gmail.com>
2022-04-13 02:36:01 +1200

    made use of react-confetti and also added some new functions

--- src/App.js
+++ src/App.js
@@ -3,27 +3,53 @@ import ReactDOM from "react-dom"
 import './index.css';
 import Die from "../src/components/die"
 import {nanoid} from "nanoid";
+import Confetti from "react-confetti"
 
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
 
-  function allNewDice(){
+function generateDice(){
+  return (
+    {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  )
+}
+
+React.useEffect(() => {
+  const allHeld = dice.every(die => die.isHeld)
+  const firstValue = dice[0].value
+  const allSameValue = dice.every(die => die.value === firstValue)
+
+  if(allHeld && allSameValue){
+    setTenzies(true)
+    console.log("You won")
+  }
+}, [dice])
+
+function allNewDice(){
     let newDice =[]
     for (let i=0; i < 10; i++){
-      newDice.push({
-        value: Math.floor(Math.random() * 6) + 1,
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(generateDice())
         
     }
       return newDice
   }
  
 
- function rollDice(){
-   setDice(allNewDice())
+function rollDice(){
+  if(tenzies){
+    setDice(allNewDice())
+  }else {
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld? die: generateDice()
+    }))
+  }
+  
  }
  
 function holdDice(id){
@@ -32,6 +58,8 @@ function holdDice(id){
   }))
 
 }
+
+
  const diceElements = dice.map(die => (
  <Die 
  key={die.id} 
@@ -41,13 +69,16 @@ function holdDice(id){
  />))
   return (
     <main>
+        {tenzies && <Confetti></Confetti>}
+        <h1>Tenzies</h1>
+        <p>Roll untill all dice are the same. Click each die to freeze it as its current value between rolls.</p>
         <div className='die-container'>
             {diceElements}
         </div>
         <button 
         className='roll-btn'
         onClick={rollDice}
-        >Roll</button>
+        >{tenzies? "New Game": "Roll" }</button>
     </main>
   );
 }
