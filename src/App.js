2fc01ab45d3cb15b3adf6cce92b5ef2758d483b2
smarist <stmarisnwokike@gmail.com>
2022-04-12 20:12:03 +1200

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
+import ReactDOM from "react-dom"
 import './index.css';
+import Die from "../src/components/die"
+
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice(){
+    let newDice =[]
+    for (let i=0; i < 10; i++){
+      newDice.push(Math.floor(Math.random() * 6) + 1)
+    }
+      return newDice
+  }
+ console.log(allNewDice())
+
+ 
+
+ const diceElements = dice.map(die => <Die value={die}/>)
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <main>
+        <div className='die-container'>
+            {diceElements}
+        </div>
+        
+    </main>
   );
 }
 
