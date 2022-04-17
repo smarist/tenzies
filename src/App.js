6f55eada97e03fca82551f329e22e335264bb920
smarist <stmarisnwokike@gmail.com>
2022-04-18 08:08:55 +1200

    added a timer function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from "react-dom"
 import './index.css';
 import Die from "../src/components/die"
 import {nanoid} from "nanoid";
 import Confetti from "react-confetti"
+import Timer from './components/timer';
 
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice())
   const [tenzies, setTenzies] = React.useState(false)
+  const [score, setScore] = React.useState(0)
 
 function generateDice(){
   return (
@@ -20,14 +21,29 @@ function generateDice(){
   )
 }
 
-React.useEffect(() => {
-  const allHeld = dice.every(die => die.isHeld)
-  const firstValue = dice[0].value
-  const allSameValue = dice.every(die => die.value === firstValue)
+const allHeld = dice.every(die => die.isHeld)
+const firstValue = dice[0].value
+const allSameValue = dice.every(die => die.value === firstValue)
 
+React.useEffect(() => {
   if(allHeld && allSameValue){
     setTenzies(true)
     console.log("You won")
+     if(firstValue === 6){
+       setScore(prevScore => 60)
+     }else if(firstValue === 5){
+      setScore(prevScore => 50)
+     }else if(firstValue === 4){
+      setScore(prevScore => 40)
+     }else if(firstValue === 3){
+      setScore(prevScore => 30)
+     }else if(firstValue === 2){
+      setScore(prevScore => 20)
+     }else if(firstValue === 1){
+      setScore(prevScore => 10)
+     }
+  } else {
+    setScore(prevScore => 0)
   }
 }, [dice])
 
@@ -40,17 +56,18 @@ function allNewDice(){
       return newDice
   }
  
-
+console.log(score)
 function rollDice(){
-  if(tenzies){
-    setDice(allNewDice())
-  }else {
+  if(!tenzies){
     setDice(oldDice => oldDice.map(die => {
       return die.isHeld? die: generateDice()
-    }))
-  }
+  }))
+} else {
+  setTenzies(false)
+  setDice(allNewDice())
+} 
   
- }
+}
  
 function holdDice(id){
   setDice(oldDice => oldDice.map(die => {
@@ -69,9 +86,17 @@ function holdDice(id){
  />))
   return (
     <main>
+        <Timer/>
         {tenzies && <Confetti></Confetti>}
-        <h1>Tenzies</h1>
-        <p>Roll untill all dice are the same. Click each die to freeze it as its current value between rolls.</p>
+        {tenzies && 
+        <div className='win-section'>
+          <h3 className='won'>You won !!!</h3>
+          <h3 className='score'>You scored {score}</h3>
+        </div>
+        }
+        {!allSameValue && allHeld && <div><h3 className='wrong'>Wrong match, read the rules again</h3></div>}
+        <h1 className='title'>Tenzies</h1>
+        <p className='instructions'>Roll untill all dice are the same. Click each die to freeze it as its current value between rolls.</p>
         <div className='die-container'>
             {diceElements}
         </div>
@@ -79,6 +104,8 @@ function holdDice(id){
         className='roll-btn'
         onClick={rollDice}
         >{tenzies? "New Game": "Roll" }</button>
+        {tenzies && <button className='save-btn'>Save score</button>}
+        
     </main>
   );
 }
